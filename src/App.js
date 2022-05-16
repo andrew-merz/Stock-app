da618c8cd8162f2479a59df02e3b38c45f87ecda
andrew-merz <merz.school@gmail.com>
2022-05-16 13:26:56 -0400

    new api linked and displaying on page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const apiKey = process.env.REACT_APP_STOCK_API_KEY;
+  const url = `https://api.stockdata.org/v1/data/quote?symbols=AAPL%2CTSLA%2CMSFT&api_token=YHkmnmMscvsSB7bwvZA4zxCEBYXVpoumVTRWoJtV`;
+  const [stock, setStock] = useState(null);
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setStock(response.data);
+      console.log(stock.data[0].name);
+    });
+  }, [url]);
+
+  if (stock) {
+    return (
+      <div>
+        <h1>{stock.data[0].name}</h1>
+      </div>
+    );
+  }
   return (
     <div className="App">
       <h1>Stock App</h1>
